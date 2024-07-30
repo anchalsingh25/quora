9f115de10d0263b38ba727e6ff280d9416007d30
Anchal Singh <77047523+anchalsingh25@users.noreply.github.com>
2024-07-30 13:09:24 +0530

    question owner updated (#2)
    * question owner updated
    
    * updated the access of id
    
    * updated reference in model

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,10 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: %i[update destroy]
+  before_action :user_auth, only: %i[create update destroy]
 
   def create
     question = Question.new(question_param)
+    question.user_id = @current_user.id
     return render json: { message: question.errors }, status: :unprocessable_entity unless question.save
 
     render json: question, status: :created
@@ -13,12 +15,17 @@ class QuestionsController < ApplicationController
     render json: questions
   end
 
-  def set_question
-    @question = Question.find(params[:id])
-    return render json: { message: 'Question not found' }, status: :not_found if @question.nil?
+  def show
+    question = Question.find(params[:id])
+    render json: question
   end
 
   def update
+    if @question.user_id != @current_user.id
+      return render json: { message: 'Not authorized to update the question' },
+                    status: :unauthorized
+    end
+
     unless @question.update(question_param)
       return render json: { message: 'Question updation failed' },
                     status: :unprocessable_entity
@@ -28,11 +35,21 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
+    if @question.user_id != @current_user.id
+      return render json: { message: 'Not authorized to delete the question' },
+                    status: :unauthorized
+    end
+
     @question.destroy
     render status: :no_content
   end
 
   private
+
+  def set_question
+    @question = Question.find(params[:id])
+    return render json: { message: 'Question not found' }, status: :not_found if @question.nil?
+  end
 
   def question_param
     params.require(:question).permit(:title, :description)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  has_many :questions
   has_secure_password
   validates :name, presence: true
   validates :email_id, presence: true, uniqueness: true
+  validates :password, length: { minimum: 8 }
 end
